668a1d716d86d7694382ae534b824b2dfa085a13
MaiVietQuynh <maivietquynh2001@gmail.com>
2023-03-21 14:55:18 +0700

    Entity Framework

---
+++ EF_Identity/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using EF_Identity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Identity.Pages
+{
+	public class IndexModel : PageModel
+	{
+		
+		private readonly ILogger<IndexModel> _logger;
+		private readonly MyBlogContext _MyBlogContext;
+		public IndexModel(ILogger<IndexModel> logger, MyBlogContext myBlogContext)
+		{
+			_MyBlogContext= myBlogContext;
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			var posts = (from p in _MyBlogContext.Articles 
+						orderby p.Created descending
+						select p).ToList();
+			ViewData["posts"] = posts;
+		}
+	}
+}
---
+++ EF_Identity/README.md
@@ -0,0 +1,9 @@
+# Entity Framework
+	
+## Tool
+	- Add-Migration name
+	- Update-Database
+	- Drop-Database
+## Tao du lieu mau co san
+	- Su dung thu vien Bogus(Nuget)
+	- 
